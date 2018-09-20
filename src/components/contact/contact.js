4efc620ab3f4c8036526a5604d86cbab34d20e21
raphaeljq <raphaeljq@gmail.com>
2018-09-20 01:19:44 -0300

    Mais uma tentativa de atualizar esse form de contato

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -14,7 +14,7 @@ const Contact = () => (
         <h5>Contact</h5>
         <Card>
           <Row>
-            <form action="/success" name="contact" method="POST" data-netlify="true">
+            <form action="/success" name="contact" data-netlify="true">
               <Input placeholder="lorem@ipsum.com" type="email" label="Email" s={12} />
               <Input placeholder="Lorem Ipsum..." label="Message" s={12} />
             <Col s={12} m={12}>
